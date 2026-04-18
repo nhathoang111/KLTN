c8d9a4e47684d3458c288d5caf27b4bb9ad4439e
Nhật Hoàng Nguyễn <hoangnguyenggggg11@gmail.com>
2026-04-18 21:30:00 +0700

    fix cors config

--- src/main/java/com/example/schoolmanagement/config/CorsConfig.java
+++ src/main/java/com/example/schoolmanagement/config/CorsConfig.java
@@ -1,5 +1,5 @@
 package com.example.schoolmanagement.config;
-
+//comment
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
